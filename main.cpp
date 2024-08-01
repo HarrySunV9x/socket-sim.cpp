655e8105073d00763176ea5de02e0813b414d027
HarrySunV9x <harrysunv9x@outlook.com>
2024-08-01 20:58:04 +0800

    feat: 更改项目结构，引入gtest模块
    - include代表头文件，src代表主程序，test代表测试程序
    - 拆分CMakeList，引入适合自己的模块
    - 引入gtest，lib存放外部链接库，在include存入对应的头文件

--- main.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-#include "spdlog/spdlog.h"
-
-int main(int argc, char *argv[]) {
-    spdlog::info("Welcome to info spdlog!");
-
-    return 0;
-}
---
+++ test/testMain.h
@@ -0,0 +1,14 @@
+//
+// Created by WorkHarry on 2024/8/1.
+//
+
+#ifndef SOCKET_SIM_CPP_TEST_H
+#define SOCKET_SIM_CPP_TEST_H
+
+
+class test {
+
+};
+
+
+#endif //SOCKET_SIM_CPP_TEST_H
