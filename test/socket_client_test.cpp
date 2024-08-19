e6754f77bff948271bde3953e8db05394026f8f0
HarrySunV9x <harrysunv9x@outlook.com>
2024-08-19 20:45:04 +0800

    feat: 增加Socket的简单验证
    - 增加socket init的简单验证
    - (Todo) 增加完整流程验证
    - (Todo) 增加错误处理验证

---
+++ test/socket_client_test.cpp
@@ -0,0 +1,34 @@
+#include <netinet/in.h>
+#include <memory>
+
+#include "gtest/gtest.h"
+
+#include "../src/include/socket_client.h"
+#include "../src/include/socket_base.h"
+
+// 定义测试类
+class SocketClientTest : public ::testing::Test {
+protected:
+    void SetUp() override {
+        m_socket = std::make_unique<SocketClient>();
+    }
+
+    void TearDown() override {
+    }
+
+public:
+    std::unique_ptr<SocketBase> m_socket;
+};
+
+TEST_F(SocketClientTest, InitTest) {
+    EXPECT_EQ(m_socket->Init(), 0);
+}
+TEST_F(SocketClientTest, InitSetAddressTest) {
+    EXPECT_EQ(m_socket->Init("127.1.0.2"), 0);
+}
+TEST_F(SocketClientTest, InitSetPortTest) {
+    EXPECT_EQ(m_socket->Init("127.0.0.1", "24154"), 0);
+}
+TEST_F(SocketClientTest, InitSetAddressAndPortTest) {
+    EXPECT_EQ(m_socket->Init("127.1.0.2", "24154"), 0);
+}
